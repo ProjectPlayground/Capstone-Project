76171eceb81a01aa347058525383dce5553a576b
richa-bansal <aniket@Anikets-MacBook-Pro.local>
2016-03-28 19:19:13 -0700

    New Event activity UI updates
    1. Created content_new_event.xml layout. Added event title, location,
    dates and event notes to the UI.
    2. Updated styles.xml to add a style for UI items divider
    3. Added png files in drawable folder for location icon, time icon, and
    notes icon.
    4. Updated NewEventActivity.java to attach date and time pickers to
    date/time buttons.
    5. Updated NewEventActivity.java to attach PlaceSelectionListener to
    PlaceAutoCompleteFragment.
    6. Updated strings.xml to format place details
    7. Updated AndroidManifest.xml to add meta-data tag for google geo api
    8. Updated build.gradle to include cardview and google play-services
    dependencies.

--- WannaJoin/app/src/main/java/com/village/wannajoin/wannajoin/NewEventActivity.java
+++ WannaJoin/app/src/main/java/com/village/wannajoin/wannajoin/NewEventActivity.java
@@ -1,13 +1,43 @@
 package com.village.wannajoin.wannajoin;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class NewEventActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class NewEventActivity extends AppCompatActivity implements PlaceSelectionListener {
+
+    Button mStartDate;
+    Button mEndDate;
+    Button mStartTime;
+    Button mEndTime;
+    TextView mlocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +46,117 @@ public class NewEventActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //initialize date and time with current time
+
+        mStartDate = (Button)findViewById(R.id.event_start_date);
+        mEndDate = (Button)findViewById(R.id.event_end_date);
+        mStartTime = (Button)findViewById(R.id.event_start_time);
+        mEndTime = (Button)findViewById(R.id.event_end_time);
+        mlocation = (TextView)findViewById(R.id.event_location);
+        final Calendar c = Calendar.getInstance();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE MMM d, yyyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:m a");
+        mStartDate.setText(dateFormat.format(c.getTime()));
+        mEndDate.setText(dateFormat.format(c.getTime()));
+        mStartTime.setText(timeFormat.format(c.getTime()));
+        mEndTime.setText(timeFormat.format(c.getTime()));
+        // Retrieve the PlaceAutocompleteFragment.
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        autocompleteFragment.setHint("Search location");
+
+        // Register a listener to receive callbacks when a place has been selected or an error has
+        // occurred.
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+
+    /**
+     * Callback invoked when a place has been selected from the PlaceAutocompleteFragment.
+     */
+    @Override
+    public void onPlaceSelected(Place place) {
+        Log.d("RB",place.toString());
+       Log.d("RB", formatPlaceDetails(getResources(), place.getName(), place.getId(),
+               place.getAddress(), place.getPhoneNumber(), place.getWebsiteUri()).toString()) ;
+
+        mlocation.setText(formatPlaceDetails(getResources(), place.getName(), place.getId(),
+                place.getAddress(), place.getPhoneNumber(), place.getWebsiteUri()).toString());
+
+    }
+
+    /**
+     * Callback invoked when PlaceAutocompleteFragment encounters an error.
+     */
+    @Override
+    public void onError(Status status) {
+        Log.d("RB", "onError: Status = " + status.toString());
+
+        Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Helper method to format information about a place nicely.
+     */
+    private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
+                                              CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+        Log.e("RB", res.getString(R.string.place_details, name, address, phoneNumber,
+                websiteUri));
+        return Html.fromHtml(res.getString(R.string.place_details, name, address, phoneNumber,
+                websiteUri));
 
     }
 
